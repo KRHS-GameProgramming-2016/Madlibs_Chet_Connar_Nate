d2a5aa0980dd7082c4dc2b5520d038bb3b46cc7f
rfew <ssdfsdf@dsfsd.com>
2016-10-11 12:00:12 -0400

    getword is somehow not working, figure it out next class

--- story1.py
+++ story1.py
@@ -5,12 +5,12 @@ def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
     food1 = getword ("enter food" )
-    
+    pokemon = getword ("enter a pokemon")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like " + temperature1
     text += " outside."    
-    text +=" " 
+    text +=" Then suddenly, a" + pokemon1
     text +=" " 
     
     return text
