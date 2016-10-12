f17fc8045cd07f5ff280ea02b04268e91cb09103
rfew <ssdfsdf@dsfsd.com>
2016-10-12 11:34:51 -0400

    Changed STory written by names.

--- story1.py
+++ story1.py
@@ -1,6 +1,6 @@
 from input import *
 
-#Written by connar dunn
+#Written by Nathan, Chett, and Connar
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
@@ -10,8 +10,9 @@ def story():
     text += "One day I went to the " + location1
     text += ". It was like " + temperature1
     text += " outside."    
-    text +=" Then suddenly, a" + pokemon1
-    text +=" " 
+    text +=" Then suddenly, a " + pokemon1
+    text +=" appeared! " 
+    text +=""
     
     return text
     
