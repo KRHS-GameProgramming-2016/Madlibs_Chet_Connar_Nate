999c24354531ec29dc22d935c56c169400790c02
rfew <ssdfsdf@dsfsd.com>
2016-10-12 11:43:44 -0400

    Finished first story.

--- story1.py
+++ story1.py
@@ -1,18 +1,20 @@
 from input import *
 
-#Written by Nathan, Chett, and Connar
+#Written by Nathan, Chett, and 
 def story():
-    location1 = getWord("Enter a location: ")
-    temperature1 = getNumber("Enter a Number: ")
-    food1 = getWord ("enter food" )
-    pokemon1 =getWord("enter a pokemon")
+    location1 = getWord(" Enter a location: ")
+    temperature1 = getNumber(" Enter a Number: ")
+    food1 = getWord (" enter food " )
+    pokemon1 =getWord(" enter a pokemon ")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like " + temperature1
     text += " outside."    
-    text +=" Then suddenly, a " + pokemon1
+    text +=" Then suddenly, a wild " + pokemon1
     text +=" appeared! " 
-    text +=""
+    text +="While the pokemon ate all of the " + food1
+    text +=" We all watched it wondering how a random pokemon got here"
+    text +=" while the pokemon ate the food we continued doing normal things." 
     
     return text
     
