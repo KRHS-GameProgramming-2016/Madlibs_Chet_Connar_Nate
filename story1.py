7963176662b66ec38e4cd35b06ff71b50dd0b923
rfew <ssdfsdf@dsfsd.com>
2016-10-12 11:28:01 -0400

    Forgot to capitalize the w.

--- story1.py
+++ story1.py
@@ -4,8 +4,8 @@ from input import *
 def story():
     location1 = getWord("Enter a location: ")
     temperature1 = getNumber("Enter a Number: ")
-    food1 = getword ("enter food" )
-    pokemon =getword("enter a pokemon")
+    food1 = getWord ("enter food" )
+    pokemon =getWord("enter a pokemon")
     text = ""
     text += "One day I went to the " + location1
     text += ". It was like " + temperature1
