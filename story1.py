ee38f4fac12310e6cf3dc59832ce542f26afddb5
rfew <ssdfsdf@dsfsd.com>
2016-10-13 10:54:20 -0400

    Added 1 line with adjective to the story, it was completely pointless.

--- story1.py
+++ story1.py
@@ -4,8 +4,9 @@ from input import *
 def story():
     location1 = getWord(" Enter a location: ")
     temperature1 = getNumber(" Enter a Number: ")
-    food1 = getWord (" enter food " )
-    pokemon1 =getWord(" enter a pokemon ")
+    food1 = getWord (" enter food: " )
+    pokemon1 =getWord(" enter a pokemon: ")
+    adjective1 =getword " enter a adjective: "
     text = ""
     text += "One day I went to " + location1
     text += ". It was like " + temperature1
@@ -17,6 +18,6 @@ def story():
     text +=" We all watched it wondering how it got here."
     text +=" While the pokemon ate the " + food1
     text += " we continued doing normal things." 
-    
+    text += " These things where very " + adjective1
     return text
     
