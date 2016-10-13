1901a838e87c823094ae49d0c78410d71b3532bb
rfew <ssdfsdf@dsfsd.com>
2016-10-13 10:42:58 -0400

    made story better

--- story1.py
+++ story1.py
@@ -7,14 +7,16 @@ def story():
     food1 = getWord (" enter food " )
     pokemon1 =getWord(" enter a pokemon ")
     text = ""
-    text += "One day I went to the " + location1
+    text += "One day I went to " + location1
     text += ". It was like " + temperature1
     text += " outside."    
     text +=" Then suddenly, a wild " + pokemon1
     text +=" appeared! " 
-    text +="While the pokemon ate all of the " + food1
-    text +=" We all watched it wondering how a random pokemon got here."
-    text +=" While the pokemon ate the food we continued doing normal things." 
+    text +="While the " + pokemon1
+    text+= " ate all of the " + food1
+    text +=" We all watched it wondering how it got here."
+    text +=" While the pokemon ate the " + pokemon1
+    text += "food we continued doing normal things." 
     
     return text
     
