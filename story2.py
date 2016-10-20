f2ee67ae3a0df035061cabe83f26e7ec0730e522
rfew <ssdfsdf@dsfsd.com>
2016-10-20 11:16:55 -0400

    we need a story four, im making the 2nd one

--- story2.py
+++ story2.py
@@ -1,12 +1,21 @@
 from input import *
 
-#Written by Chett
+#Written by Nathan
 def story():
-    location1 = getWord("Enter a location: ")
+    location1 = getWord (" Enter a location: ")
+    food1 = getWord (" Enter a food: ")
+    adjective1 = getWord (" Enter a adjective: ")
+    verb1 = getWord (" Enetr a verb: ")
+    noun1 = getWord (" Enter a noun: ")
+    
     
     text = ""
-    text +=" I went over to " + location1
-    
+    text += " I went over to " + location1
+    text += " and decided that I had a razy craving for " + food1
+    text += ". While eating the " + adjective1
+    text += " I decided to start " + verb1
+    text += " the " + noun1
+    text += ". "
     
     return text
     
