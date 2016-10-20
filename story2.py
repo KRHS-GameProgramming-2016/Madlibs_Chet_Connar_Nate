cc75c24ead228fcb9e49fe478d7b118d754546f4
rfew <ssdfsdf@dsfsd.com>
2016-10-20 11:25:08 -0400

    finished second story, hasn't been proofread.

--- story2.py
+++ story2.py
@@ -7,6 +7,10 @@ def story():
     adjective1 = getWord (" Enter a adjective: ")
     verb1 = getWord (" Enetr a verb: ")
     noun1 = getWord (" Enter a noun: ")
+    opponent1 = getWord (" Enter an opponent (who has arms): ")
+    adjective2 = getWord (" Enter a second adjective: ")
+    bodyofwater1 = getWord (" Enter a body of water: ")
+    food2 = getword (" Enter a second food item: ")
     
     
     text = ""
@@ -15,7 +19,13 @@ def story():
     text += ". While eating the " + adjective1
     text += " I decided to start " + verb1
     text += " the " + noun1
-    text += ". "
+    text += ". After I got bored doing that thing, I headed on over to the " + location2
+    text += " to start lifting weights and stuff. "
+    text += "After I beat " + opponent1
+    text += " at arm wrestling I started to look " + adjective2
+    text += " at the " bodyofwater1
+    text += " I wondered how in the world I didnt have any " + food2
+    text += " left."
     
     return text
     
