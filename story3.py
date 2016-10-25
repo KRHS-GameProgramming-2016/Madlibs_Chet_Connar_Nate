8efe0978c4588031f5d585c3e8459055f5fc68e7
rfew <ssdfsdf@dsfsd.com>
2016-10-25 11:37:14 -0400

    story 3 proofread almost thoroughly.

--- story3.py
+++ story3.py
@@ -6,7 +6,8 @@ def story():
     emotion1 = getWord (" Enter a emotion: ")
     emotion2 = getWord (" Enter a second emotion: ")
     noun2 = getWord (" Enter a second noun: ")
-    gender1 = getWord (" Enter your gender: ")
+    gender1 = getWord (" Enter man or women: ")
+    noun3 = getWord (" Enter a third noun: ")
     
     text =""
     text += " I am a " + noun1
@@ -17,8 +18,8 @@ def story():
     text += "Every once in a while a " + noun2
     text += " came along to make sure I was " + emotion2
     text += ". " + noun2
-    text += " was doing good but sometimes didnt do good and I was very " + emotion2
-    text += ". While that happened in a " + noun3
+    text += " was doing good but sometimes didnt do good and I was not very " + emotion2
+    text += ". While that happened, in a " + noun3
     text += " far far away in a place we dont really understand. "
     text += "there was nothing, because those kinds of things do not exist. get real " + gender1
     text += ". "
