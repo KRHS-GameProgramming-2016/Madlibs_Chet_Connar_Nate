399dd26abc577f55b8f2bc10c6605f76e903d427
rfew <ssdfsdf@dsfsd.com>
2016-10-26 11:42:03 -0400

    fixed many spelling mistakes.

--- story3.py
+++ story3.py
@@ -7,14 +7,16 @@ def story():
     game1 = getWord (" Enter a Game Name: ")
     friendname1 = getWord (" Enter a Name: ")
     classname = getClass (" Enter a titanfall class (Oger, Atlas, or Strider): ")
+    number1 = getNumber (" Enter a number: ")
     
     text =""
     text += " I am a " + noun1 
-    text += ".  one day play" + game1
-    text += " with my frined, " + friendname1
-    text += " and i dead. I the ne-spawned and killed " + friendname1
-    text += " I left the game to go play titanfall.  i paly as a " + classname
-       
+    text += ".  one day I play " + game1
+    text += " with my friend, " + friendname1
+    text += " who killed me. I then re-spawned and killed " + friendname1
+    text += " I then left the game to go play some titanfall.  I play as a " + classname
+    text += ". I have a " + number1
+    text += " to 1 k/d ration. "
     
     return text
   
